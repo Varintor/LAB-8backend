c1e16bfa2888f4860c2a164721073004a7163e27
Varintor <varintorn_si@cmu.ac.th>
2025-09-27 14:46:25 +0700

    LAB9 6.2 finish

--- src/main/java/se331/lab/controller/OrganizerController.java
+++ src/main/java/se331/lab/controller/OrganizerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se331.lab.service.OrganizerService;
+import se331.lab.util.LabMapper;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +14,7 @@ public class OrganizerController {
 
     @GetMapping("/organizers")
     public ResponseEntity<?> getOrganizers() {
-        return ResponseEntity.ok(organizerService.getAllOrganizer());
+        return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(organizerService.getAllOrganizer()) );
     }
 }
 
--- src/main/java/se331/lab/util/LabMapper.java
+++ src/main/java/se331/lab/util/LabMapper.java
@@ -4,6 +4,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import se331.lab.entity.Event;
 import se331.lab.entity.EventDTO;
+import se331.lab.entity.Organizer;
+import se331.lab.entity.OrganizerDTO;
 
 import java.util.List;
 
@@ -14,4 +16,6 @@ public interface LabMapper {
     EventDTO getEventDTO(Event event);
 
     List<EventDTO> getEventDTOs(List<Event> events);
+    OrganizerDTO getOrganizerDTO(Organizer organizer);
+    List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
 }
